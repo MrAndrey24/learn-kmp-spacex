f4d527b71330d6be0de345adf2336ac17ebd8089
Andrey Acosta <andreyacostar@gmail.com>
2026-02-19 14:40:51 -0600

    fix(paso 02)

---
+++ shared/src/commonMain/kotlin/compose/project/demo/composedemo/di/modules/DataModule.kt
@@ -0,0 +1,11 @@
+package compose.project.demo.composedemo.di.modules
+
+import compose.project.demo.composedemo.data.remote.IRemoteRocketLaunchesDataSource
+import compose.project.demo.composedemo.data.remote.RemoteRocketLaunchesDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import org.koin.dsl.module
+
+val dataModule = module {
+    single<IRemoteRocketLaunchesDataSource> { RemoteRocketLaunchesDataSource(get(), Dispatchers.IO) }
+}
